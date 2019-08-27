e4149169c9b54ecd6035f194393277880975807d
Erick Robles <erickrobles135@gmail.com>
2019-08-27 13:35:17 -0500

    actualizado en clase

--- arreglos/main.js
+++ arreglos/main.js
@@ -7,6 +7,7 @@ let b = [1, 5, 7, 4]
 
 
 console.log(getMin(b))
+console.log(getMax(b))
 
 function getMin(a) {
     if (a.length == 0) return -1
@@ -18,4 +19,15 @@ function getMin(a) {
 }
 
 
+function getMax(a) {
+    let max = nota(a[0])
+    for(let i = 0; i < a.length; i++) {
+        if (nota(a[i]) > max) max = nota(a[i])
+    }
+    return max
+}
+
+function nota(n){
+ return notas[n]
+}
 
